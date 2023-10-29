3876cc77d953dbc02420b223ef9aeb101be2125d
BakirovDanil <danil.bakiroff84@gmail.com>
2023-10-29 20:56:42 +0300

    Допилил приложуху

--- Calculation.py
+++ Calculation.py
@@ -8,14 +8,16 @@ def Raschet(firstCu, secondCu, maxCu, minCu):
         result = ((1 - firstCu1)*secondCu2 + firstCu1)
         if result > maxCu.get():
             showinfo("Вывод", "Вывод: прибыль растет. Полученный КУ: " + str(result))
-        elif result > minCu.get():
+        elif result < minCu.get():
             showinfo("Вывод", "Вывод: прибыль падает. Полученный КУ: " + str(result))
-        elif minCu < result < maxCu:
+        elif minCu.get() < result < maxCu.get():
             showinfo("Вывод", "Вывод: система не может сделать вывод, т.к. значение находится между граничными КУ. Полученный КУ: " + str(result))
     else:
         showerror(title="Ошибка", message="Введенные данные некорректны")
         firstCu.set(0.0)
         secondCu.set(0.0)
+        maxCu.set(0.0)
+        minCu.set(0.0)
 
 
 # !!!!ЮЗАТЬ В ХОДЕ ВЫПОЛНЕНИЯ КУРСОВОГО ПРОЕКТА ДОЛБЕНЬ!!!!
