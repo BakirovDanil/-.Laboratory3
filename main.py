1ec313d00e43c8d397dd5aaa59d11aad7422f24c
BakirovDanil <danil.bakiroff84@gmail.com>
2023-10-29 16:40:42 +0300

    Добавил новый модуль и отрисовку объектов

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from tkinter import*
+from tkinter import *
 from tkinter import ttk
 
+import Object
 
-Frame=Tk()
+Frame = Tk()
 
 
+def Finish():
+    Frame.destroy()
+    print("Приложение закрыто")
+
+
+def MainForm(window):
+    window.title("Бакиров Данил, Валеев Марат. Задача с коэффициентом уверенности")
+    window.geometry("600x700+400+200")
+    window.resizable(False, False)
+    window.protocol("WM_DELETE_WINDOW", Finish)
+    Label1 = Object.Label()
+    Label1.Sozdanie(window)
+    window.mainloop()
+
+
+MainForm(Frame)
